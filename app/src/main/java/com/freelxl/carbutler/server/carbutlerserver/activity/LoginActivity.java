b8fc6b61ea693a1ffbf73bb8507a5807aa2a4af6
lixiuliang <appmobiledevelop@126.com>
2015-04-06 22:05:08 +0800

    提交沟通问题

--- app/src/main/java/com/freelxl/carbutler/server/carbutlerserver/activity/LoginActivity.java
+++ app/src/main/java/com/freelxl/carbutler/server/carbutlerserver/activity/LoginActivity.java
@@ -3,15 +3,19 @@ package com.freelxl.carbutler.server.carbutlerserver.activity;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
 import com.baidu.android.pushservice.PushConstants;
 import com.baidu.android.pushservice.PushManager;
 import com.freelxl.baselibrary.utils.IntentUtils;
+import com.freelxl.baselibrary.utils.MD5Util;
 import com.freelxl.carbutler.server.carbutlerserver.R;
 import com.lidroid.xutils.ViewUtils;
 import com.lidroid.xutils.view.annotation.event.OnClick;
+
+import java.util.ArrayList;
 
 public class LoginActivity extends Activity {
 
@@ -26,7 +30,16 @@ public class LoginActivity extends Activity {
 //                PushConstants.LOGIN_TYPE_API_KEY,
 //                PushUtils.getMetaValue(this, "4Bac80ruzeGfTXEVne2p5oGM"));
 
-        PushManager.startWork(getApplicationContext(),PushConstants.LOGIN_TYPE_API_KEY,"4Bac80ruzeGfTXEVne2p5oGM");
+        PushManager.startWork(getApplicationContext(), PushConstants.LOGIN_TYPE_API_KEY, "4Bac80ruzeGfTXEVne2p5oGM");
+        String a = MD5Util.getMD5Str("3");
+
+        String b = MD5Util.getMD5Str("3A");
+//        System.out.print("tag----" + a + "+++++" + b);
+        Log.d("3A", "-----------" + a + "--------" + b);
+        ArrayList<String> strings = new ArrayList<>();
+        strings.add(a);
+        strings.add(b);
+        PushManager.setTags(this, strings);
 
     }
 
